8311a8a4bde79c1494e6c60189683033ce47d043
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2022-11-09 21:03:41 +0000

    [Add] 4-課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to '/top'
+  end
+
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  get 'home/about'
+  root to: "home#top"
   devise_for :users
-
+  post 'users' => 'users#create'
   resources :users, only: [:show, :edit]
   resources :books, only: [:new, :index, :show, :edit]
 
