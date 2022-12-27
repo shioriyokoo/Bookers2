a223876841be2c8eebf8530021c52d7670996631
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2022-12-27 09:09:24 +0000

    [Updata] 4-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @user = current_user
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,12 @@ has_one_attached :profile_image
   validates :introduction, length: { maximum: 50 }
 
 
+
+
 def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
   end
     profile_image.variant(resize_to_limit: [width, height]).processed
 end
